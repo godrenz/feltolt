7c1ab1d614896b4f9e07dddf6ea4298b8347b933
Your Name <godrenz@gmail.com>
2011-04-06 10:25:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feltolt::Application.config.secret_token = '1da63060084f38de747abf7d410f04656f3fa0f0c81c7eff745899dc447b0d4a072a84aa71e6c283a93a8e021c84366c2a784cbf94e2bc111db37a5821651b2d'
